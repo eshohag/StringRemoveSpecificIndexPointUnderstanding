f7320c708c98b8b6e109f026f5e833f93ca2f95c
mdshohagmia <shohaghassan14@hotmail.com>
2018-12-05 21:12:19 +0600

    Initial Commit

--- SubStringUnderstanding/Program.cs
+++ SubStringUnderstanding/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubStringUnderstanding
+namespace StringRemoveUnderstanding
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string valueOfTest = "19260001";
+            int originalBabyId = Convert.ToInt32(valueOfTest.Remove(0,4));
+            Console.WriteLine(originalBabyId);
+            Console.ReadKey();
         }
     }
 }
